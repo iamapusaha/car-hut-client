ca31f1267b89d0f765c80adb697f7cdce34d6d54
iamapusaha <apusaha392@gmail.com>
2023-10-21 13:46:59 +0600

    update brand card

--- src/pages/BrandItems/BrandItems.jsx
+++ src/pages/BrandItems/BrandItems.jsx
@@ -7,12 +7,22 @@ const BrandItems = ({ brand }) => {
     const { name, photo } = brand;
 
     return (
+        // <Link to={`/product/${name}`}>
+        //     < div className='border-2 rounded border-yellow-400'>
+        //         <img className='w-52 h-52' src={photo} alt="" />
+        //         <p>{name}</p>
+        //     </div >
+        // </Link >
         <Link to={`/product/${name}`}>
-            < div className='border-2 rounded border-yellow-400'>
-                <img className='w-52 h-52' src={photo} alt="" />
-                <p>{name}</p>
-            </div >
-        </Link >
+            <div className="card min-w-96 bg-base-100 shadow-xl">
+                <figure className="px-10 pt-10">
+                    <img src={photo} alt="Shoes" className="rounded-xl h-20" />
+                </figure>
+                <div className="card-body items-center text-center">
+                    <h2 className="card-title">{name}</h2>
+                </div>
+            </div>
+        </Link>
     );
 };
 
